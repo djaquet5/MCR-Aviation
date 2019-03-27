9318b1a052dbcae9387fcdf64bffd98b08da33eb
yoann0000 <47739072+yoann0000@users.noreply.github.com>
2019-03-27 16:55:37 +0100

    Stuff

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,13 @@
-public class Client implements ClientStatus{
+import java.util.LinkedList;
+
+public class Client {
     private static int _id = 0;
 
     private String surname;
     private String name;
     private int id;
     private int lastAction;
+    public static LinkedList observers = new LinkedList<Observer>();
 
     public Client(String surname, String name) {
         this.surname = surname;
@@ -13,5 +16,35 @@ public class Client implements ClientStatus{
         lastAction = 0;
     }
 
+    public String getSurname() {
+        return surname;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getLastAction() {
+        return lastAction;
+    }
+
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void deleteObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void deleteObservers() {
+        observers.clear();
+    }
+
+    public void notifyObservers(Object arg) {
+        
+    }
 }
